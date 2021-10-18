29d87df0ecf9a912fca25b162b25739a677d2254
Seweryn Woźniak <sewerynwozniak111$gmail.com>
2021-10-18 09:28:16 +0200

    adding restart method

--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-
 class Memory{
 
 
     constructor(){
         this.cards = document.querySelectorAll('.card')
         this.fronts = document.querySelectorAll('.front')
-        this.frontsArray = [0,1,2,3]
+        this.backs = document.querySelectorAll('.back')
+        this.modal = document.querySelector('.modal')
+        this.restartBtn = document.querySelector('.restartBtn')
+        this.modal__number = document.querySelector('.modal__number')
+        this.modalBtn = document.querySelector('.modal__btn')
+        this.numberOfMoves = document.querySelector('.moves__nr')
+        this.frontsArray=[]
+        this.cards.forEach((card,i)=>this.frontsArray.push(i))
         this.moves=0
         this.colorsArray = [
-            'red', 'blue'
+            'red', 'royalblue', 'yellow', 'violet', 'limegreen', 'pink', 'orange', 'ghostwhite', 'slateblue', 'palegreen'
         ]
         this.assignColor()
         this.arrayOfCheckingPair = []
-        this.isChecking = false;
+        this.stopTheGame = false;
         this.events()
     }
 
 
     events(){
         this.cards.forEach(card=>card.addEventListener('click', ()=>this.checkCard(card)))
+        this.modalBtn.addEventListener('click', ()=>this.closeModal())
+        this.restartBtn.addEventListener('click', ()=>this.restart())
+    }
+
+    restart(){
+        this.moves=-1
+        this.stopTheGame = false;
+        this.updateNumberOfMoves()
+        this.fronts.forEach(front=>front.classList.remove('showFront'))
+        this.backs.forEach(back=>back.classList.remove('hideBack'))     
+        this.colorsArray = [
+            'red', 'royalblue', 'yellow', 'violet', 'limegreen', 'pink', 'orange', 'ghostwhite', 'slateblue', 'palegreen'
+        ]
+        this.fronts.forEach(front=>front.style.backgroundColor='none')
+        this.frontsArray=[]
+        this.cards.forEach((card,i)=>this.frontsArray.push(i))
+        this.assignColor()
+    }
+
+    showModal(){
+        this.modal.style.transform = 'translate(-50%,-50%) scale(1)'
+    }
+
+    closeModal(){
+        this.modal.style.transform = 'translate(-50%,-50%) scale(0)'
     }
 
     checkCard(card){
         
-        if(this.isChecking) return
+        if(this.stopTheGame){
+            return;
+        } 
+
 
         const back = card.children[0]
         const front = card.children[1]
@@ -44,18 +78,18 @@ class Memory{
 
     checkIfMatch(){
 
-        this.isChecking=true;
-
+        this.stopTheGame=true;
         const cardColor1 = this.arrayOfCheckingPair[0].children[1].style.backgroundColor;
         const cardColor2 = this.arrayOfCheckingPair[1].children[1].style.backgroundColor;
+        this.updateNumberOfMoves()
 
         if(cardColor1==cardColor2){
-            this.removeMatchedColorFromArray(cardColor1)
-            this.isChecking=false;
+            this.stopTheGame=false;
+            this.removeMatchedColorFromArray(cardColor1)         
             this.arrayOfCheckingPair = []
         }else{
             setTimeout( ()=>{this.arrayOfCheckingPair.forEach(card=>{
-                this.isChecking=false;
+                this.stopTheGame=false;
                 this.flipBackUnmatchedCard(card)            
             })}, 1500)
             setTimeout( ()=>{this.arrayOfCheckingPair = []}, 1501)
@@ -63,15 +97,25 @@ class Memory{
         }
     }
 
+    updateNumberOfMoves(){
+        this.moves++;
+        this.numberOfMoves.innerText = this.moves
+        this.modal__number.innerText = this.moves
+    }
+
 
     removeMatchedColorFromArray(color){
         const indexOfMatchedColor = this.colorsArray.indexOf(color)
         this.colorsArray.splice(indexOfMatchedColor,1)
-
+        if(this.colorsArray.length==0){
+            this.finishGame()
+        }
     }
 
-    finishGame(){
 
+    finishGame(){
+        this.stopTheGame =true
+        this.showModal()    
     }
 
 
@@ -96,7 +140,7 @@ class Memory{
 
 
     assignColor(){
-        
+
         this.colorsArray.forEach(color=>{
             this.fronts[this.generateRandomFrontIndex()].style.backgroundColor=color
             this.fronts[this.generateRandomFrontIndex()].style.backgroundColor=color
@@ -111,89 +155,3 @@ class Memory{
 const memory = new Memory();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const cards = document.querySelectorAll('.card');
-// const fronts = document.querySelectorAll('.front');
-
-// const colors =['red', 'red', 'blue', 'blue'];
-
-
-
-// cards.forEach(card=>flipCard(card))
-
-
-
-// function flipCard(card){
-    
-//     card.addEventListener('click', (e)=>{
-
-//         console.log()
-
-//         e.target.parentElement.children[0].classList.add('hideBack');
-//         e.target.parentElement.children[1].classList.add('showFront');
-//         setTimeout(()=>{
-//         e.target.parentElement.children[0].classList.remove('hideBack');
-//         e.target.parentElement.children[1].classList.remove('showFront');
-//         },1000)   
-//         })
-// }
-  
-
-
-
-
-
-// function assignRandomColors(){
-
-// fronts.forEach(front=>{
-// let randomNumber =Math.floor(Math.random()*colors.length);
-// front.style.backgroundColor = colors[randomNumber] 
-// colors.splice(randomNumber,1)
-// })
-
-// }
-
-
-
-// assignRandomColors()
